c7a15c54194fd30a97df1c5af49d68e4445ad78d
Elijah Mylcev <elijahmylcev@gmail.com>
2021-06-11 19:22:28 +0300

    add people

--- src/views/People.vue
+++ src/views/People.vue
@@ -1,21 +1,26 @@
 <template>
-  <div>
-    <div v-for="item in list" :key="item.id">{{item}}</div>
-  </div>
+  <!-- <div> -->
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="..." alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  <!-- </div> -->
 </template>
 
 <script>
+  
   export default {
-    data() {
-      return {
-        list: null
-      }
-    },
-    async mounted () {
-    this.list = await this.$swapi.getAllPeople();
-      console.log(this.list);
-    },
-  }
+    
+  };
+  //   async mounted () {
+  //   // this.list = await this.$swapi.getAllPeople();
+  //   //   console.log(this.list);
+  //   // },
+  // // }
 </script>
 
 <style lang="scss" scoped>
